48bc811a36c6a60439522ddd8481b68c367ea9b6
Shreekar11 <shreekargade2004@gmail.com>
2024-04-18 00:07:37 +0530

    added cron

--- server/dist/src/app.js
+++ server/dist/src/app.js
@@ -12,9 +12,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 const express_1 = __importDefault(require("express"));
 require("dotenv").config();
 const cors = require("cors");
+const cron = require("node-cron");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(cors());
@@ -32,3 +34,39 @@ app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
 });
+// Added cron
+app.get("/ping", (req, res) => {
+    res.status(200).json("pong....");
+});
+const API_ENDPOINT = "https://sticky-notes-9hjs.onrender.com";
+const makeApiRequest = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield axios_1.default.get(API_ENDPOINT);
+        return response.data;
+    }
+    catch (err) {
+        console.error("API request failed:", err.message);
+        throw err;
+    }
+});
+const runApiRequestJob = () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("Running API request job...");
+    try {
+        const responseData = yield makeApiRequest();
+        return responseData;
+    }
+    catch (error) {
+        return null;
+    }
+});
+// Schedule the API request job to run every 15 minutes
+cron.schedule("*/15 * * * *", () => __awaiter(void 0, void 0, void 0, function* () {
+    const responseData = yield runApiRequestJob();
+    if (responseData) {
+        // Process the response data here
+        console.log("API request successful:", responseData);
+    }
+    else {
+        console.log("API request failed");
+    }
+}));
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
+import axios from "axios";
 import express, { Request, Response, Application, urlencoded } from "express";
 require("dotenv").config();
 const cors = require("cors");
+const cron = require("node-cron");
 
 const app: Application = express();
 app.use(express.json());
@@ -9,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const userRoutes = require("../routes/user.routes");
 const noteRoutes = require("../routes/note.routes");
-const adminRoutes = require("../routes/admin.routes")
+const adminRoutes = require("../routes/admin.routes");
 
 const port = process.env.PORT;
 
@@ -23,4 +25,43 @@ app.get("/", async (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
+});
+
+// Added cron
+
+app.get("/ping", (req, res) => {
+  res.status(200).json("pong....");
+});
+
+const API_ENDPOINT = "https://sticky-notes-9hjs.onrender.com";
+
+const makeApiRequest = async () => {
+  try {
+    const response = await axios.get(API_ENDPOINT);
+    return response.data;
+  } catch (err: any) {
+    console.error("API request failed:", err.message);
+    throw err;
+  }
+};
+
+const runApiRequestJob = async () => {
+  console.log("Running API request job...");
+  try {
+    const responseData = await makeApiRequest();
+    return responseData;
+  } catch (error) {
+    return null;
+  }
+};
+
+// Schedule the API request job to run every 15 minutes
+cron.schedule("*/15 * * * *", async () => {
+  const responseData = await runApiRequestJob();
+  if (responseData) {
+    // Process the response data here
+    console.log("API request successful:", responseData);
+  } else {
+    console.log("API request failed");
+  }
 });
