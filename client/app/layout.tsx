61b17b58fd08d6997c2410f9ccb41356ebc5de4b
Shreekar11 <shreekargade2004@gmail.com>
2025-02-20 20:28:40 +0530

    chore: application title updated

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
-  title: "Sticky Notes",
+  title: "Sticky Notes | Create and manage sticky notes",
   description:
     "Sticky Notes is a web application that allows users to create and manage sticky notes, and collaborate with other users. Users can create, edit, and delete their notes, as well as set the privacy of their notes to public or private. They can also view other users' notes that are public. The application includes an admin role with the ability to add, edit, and delete users' notes, view all notes, manage role changes, and promote other users to admin.",
 };
